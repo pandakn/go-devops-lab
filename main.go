dc18c9c5f18f4e61b937d5b9f32510dd86bf0ee7
pandakn <natthawut.klangyod@gmail.com>
2024-11-16 18:22:42 +0700

    feat✨: add logger pkg, change module name and go version

--- main.go
+++ main.go
@@ -2,24 +2,37 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	"io"
 	"net/http"
 	"os"
+
+	"github.com/pandakn/go-devops-lab/pkg/logger"
 )
 
 func main() {
+	logger := logger.New()
+
+	logger.Info("starting server on :8080")
+
 	http.HandleFunc("/", getHello)
 	err := http.ListenAndServe(":8080", nil)
 	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Printf("server closed\n")
+		logger.Info("server closed")
 	} else if err != nil {
-		fmt.Printf("error starting server: %s\n", err)
+		logger.Error("error starting server: %s", err)
 		os.Exit(1)
 	}
 }
 
 func getHello(w http.ResponseWriter, r *http.Request) {
+	logger := logger.New()
+
 	name := r.URL.Query().Get("name")
-	io.WriteString(w, "Hello "+name+"\n")
+	if _, err := io.WriteString(w, "Hello "+name+"\n"); err != nil {
+		logger.Error("error writing response: %s", err)
+		http.Error(w, "error writing response", http.StatusInternalServerError)
+		return
+	}
+
+	logger.Debug("response sent successfully to %s", r.RemoteAddr)
 }
